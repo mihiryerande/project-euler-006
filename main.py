81b165953669382532e2ac9dcb27523a27dc0da5
mihiryerande <mihiryerande@gmail.com>
2022-07-11 15:32:54 -0400

    Formatting and cleanup

--- main.py
+++ main.py
@@ -4,19 +4,22 @@
 # Description:
 #     The sum of the squares of the first ten natural numbers is:
 #         1^2 + 2^2 + ... + 10^2 = 385
+#
 #     The square of the sum of the first ten natural numbers is:
 #         (1 + 2 + ... + 10)^2 = 3025
 #
-#     Hence the difference between the sum of the squares of the first ten natural numbers
-#       and the square of the sum is:
+#     Hence the difference between
+#       the sum of the squares of the first ten natural numbers and
+#       the square of the sum is:
 #         3025 - 385 = 2640
 #
-#     Find the difference between the sum of the squares of the first one hundred natural numbers
-#       and the square of the sum.
+#     Find the difference between
+#       the sum of the squares of the first one hundred natural numbers and
+#       the square of the sum.
 
-def main(n):
+def main(n: int) -> int:
     """
-    Return the difference between...
+    Return the difference between
       the sum of the squares of the first `n` natural numbers and
       the square of their sum.
 
@@ -24,27 +27,32 @@ def main(n):
         n (int): Natural number
 
     Returns:
-        Difference between sum of squares and square of sum.
+        (int): Difference between sum of squares and square of sum.
 
     Raises:
         AssertError: if incorrect params are given
     """
     assert type(n) == int and n > 0
 
-    # Sum of squares equals:
-    #   n * (n+1) * (2n+1) / 6
-    # Sum of all equals:
-    #   n * (n+1) / 2
-    # Square of ^that^ sum:
-    #   ( n * (n+1) / 2 )^2
-    #   = n^2 * (n+1)^2 / 4
+    # Idea:
+    #     Formula for sum of squares of [1, n] is:
+    #         n * (n+1) * (2n+1) / 6
     #
-    # Difference simplifies down to:
-    #   (n-1) * n * (n+1) * (3n+2) / 12
+    #     Formula for sum of [1, n] is:
+    #         n * (n+1) / 2
+    #
+    #     Square of ^that^ sum is:
+    #         ( n * (n+1) / 2 )^2
+    #         = n^2 * (n+1)^2 / 4
+    #
+    #     Difference simplifies down to:
+    #         (n-1) * n * (n+1) * (3n+2) / 12
+
     return (n-1) * n * (n+1) * (3*n+2) // 12
 
 
 if __name__ == '__main__':
     num = int(input('Enter a natural number: '))
     diff = main(num)
-    print('Difference between `square of sum` and `sum of squares` for {}:\n{}'.format(num, diff))
+    print('Difference between `square of sum` and `sum of squares` for {}:'.format(num))
+    print('  {}'.format(diff))
